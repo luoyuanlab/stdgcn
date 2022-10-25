48ec6d19f8f7327cc2fc4ce2808905fc1203f74d
Yawei Li <57171335+duckliyawei@users.noreply.github.com>
2022-10-25 12:45:19 -0500

    Update model_utils.py

--- STdGCN/model_utils.py
+++ STdGCN/model_utils.py
@@ -18,26 +18,3 @@ class JSD(nn.Module):
             q_output = F.softmax(q_output)
         log_mean_output = ((p_output + q_output )/2).log()
         return (KLDivLoss(log_mean_output, p_output) + KLDivLoss(log_mean_output, q_output))/2
-    
-    
-    
-class LogCoshLoss(nn.Module):
-    def __init__(self):
-        super(LogCoshLoss, self).__init__()
-
-    def forward(self, y_t, y_prime_t):
-        ey_t = y_t - y_prime_t
-        return torch.mean(torch.log(torch.cosh(ey_t + 1e-12)))
-
-    
-    
-class CrossEntropy(nn.Module):
-    def __init__(self):
-        super(CrossEntropy, self).__init__()
-
-    def forward(self, y_pred, y_true):
-        total_loss = torch.sum(-y_true * torch.log(y_pred) - (1 - y_true) * torch.log(1 - y_pred), axis=1)
-        num_of_samples = y_pred.shape[1]
-        mean_loss = total_loss / num_of_samples
-        mean_sample_loss = mean_loss.mean()
-        return mean_sample_loss
